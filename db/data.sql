02b07e18748c84e4adb2d997881b89e204746cb7
HexHyperion <szymonjan@gmail.com>
2026-06-05 17:11:42 +0200

    chore: Add starter data to the DB, remove external port mapping for Postgres

--- db/data.sql
+++ db/data.sql
@@ -2,12 +2,10 @@
 -- PostgreSQL database dump
 --
 
-\restrict NamcZzHDp6IQ2aYfEvL4P4P5JJBpzDuRSiueDgdCX9LNIUCns4YhtZhcUSeMRqV
+\restrict 8pYyrCiRPHcwhRS1JkDfQtzaqicIec3sYuznhSSuzK7IRefNr6U3UJU5hza5O8N
 
--- Dumped from database version 18.3 (Homebrew)
--- Dumped by pg_dump version 18.2
-
--- Started on 2026-06-02 08:44:26 CEST
+-- Dumped from database version 16.14 (Debian 16.14-1.pgdg13+1)
+-- Dumped by pg_dump version 16.14 (Debian 16.14-1.pgdg13+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -25,7 +23,6 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 220 (class 1259 OID 16389)
 -- Name: boards; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -39,7 +36,6 @@ CREATE TABLE public.boards (
 ALTER TABLE public.boards OWNER TO postgres;
 
 --
--- TOC entry 219 (class 1259 OID 16388)
 -- Name: boards_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -55,8 +51,6 @@ CREATE SEQUENCE public.boards_id_seq
 ALTER SEQUENCE public.boards_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3921 (class 0 OID 0)
--- Dependencies: 219
 -- Name: boards_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -64,7 +58,6 @@ ALTER SEQUENCE public.boards_id_seq OWNED BY public.boards.id;
 
 
 --
--- TOC entry 227 (class 1259 OID 16453)
 -- Name: card_labels; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -77,7 +70,6 @@ CREATE TABLE public.card_labels (
 ALTER TABLE public.card_labels OWNER TO postgres;
 
 --
--- TOC entry 224 (class 1259 OID 16420)
 -- Name: cards; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -95,7 +87,6 @@ CREATE TABLE public.cards (
 ALTER TABLE public.cards OWNER TO postgres;
 
 --
--- TOC entry 223 (class 1259 OID 16419)
 -- Name: cards_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -111,8 +102,6 @@ CREATE SEQUENCE public.cards_id_seq
 ALTER SEQUENCE public.cards_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3922 (class 0 OID 0)
--- Dependencies: 223
 -- Name: cards_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -120,7 +109,6 @@ ALTER SEQUENCE public.cards_id_seq OWNED BY public.cards.id;
 
 
 --
--- TOC entry 222 (class 1259 OID 16400)
 -- Name: columns; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -136,7 +124,6 @@ CREATE TABLE public.columns (
 ALTER TABLE public.columns OWNER TO postgres;
 
 --
--- TOC entry 221 (class 1259 OID 16399)
 -- Name: columns_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -152,8 +139,6 @@ CREATE SEQUENCE public.columns_id_seq
 ALTER SEQUENCE public.columns_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3923 (class 0 OID 0)
--- Dependencies: 221
 -- Name: columns_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -161,7 +146,6 @@ ALTER SEQUENCE public.columns_id_seq OWNED BY public.columns.id;
 
 
 --
--- TOC entry 229 (class 1259 OID 16485)
 -- Name: comments; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -176,7 +160,6 @@ CREATE TABLE public.comments (
 ALTER TABLE public.comments OWNER TO postgres;
 
 --
--- TOC entry 228 (class 1259 OID 16484)
 -- Name: comments_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -192,8 +175,6 @@ CREATE SEQUENCE public.comments_id_seq
 ALTER SEQUENCE public.comments_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3924 (class 0 OID 0)
--- Dependencies: 228
 -- Name: comments_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -201,7 +182,6 @@ ALTER SEQUENCE public.comments_id_seq OWNED BY public.comments.id;
 
 
 --
--- TOC entry 226 (class 1259 OID 16442)
 -- Name: labels; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -217,7 +197,6 @@ CREATE TABLE public.labels (
 ALTER TABLE public.labels OWNER TO postgres;
 
 --
--- TOC entry 225 (class 1259 OID 16441)
 -- Name: labels_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -233,8 +212,6 @@ CREATE SEQUENCE public.labels_id_seq
 ALTER SEQUENCE public.labels_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3925 (class 0 OID 0)
--- Dependencies: 225
 -- Name: labels_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -242,7 +219,6 @@ ALTER SEQUENCE public.labels_id_seq OWNED BY public.labels.id;
 
 
 --
--- TOC entry 3719 (class 2604 OID 16392)
 -- Name: boards id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -250,7 +226,6 @@ ALTER TABLE ONLY public.boards ALTER COLUMN id SET DEFAULT nextval('public.board
 
 
 --
--- TOC entry 3723 (class 2604 OID 16423)
 -- Name: cards id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -258,7 +233,6 @@ ALTER TABLE ONLY public.cards ALTER COLUMN id SET DEFAULT nextval('public.cards_
 
 
 --
--- TOC entry 3721 (class 2604 OID 16403)
 -- Name: columns id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -266,7 +240,6 @@ ALTER TABLE ONLY public.columns ALTER COLUMN id SET DEFAULT nextval('public.colu
 
 
 --
--- TOC entry 3727 (class 2604 OID 16488)
 -- Name: comments id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -274,7 +247,6 @@ ALTER TABLE ONLY public.comments ALTER COLUMN id SET DEFAULT nextval('public.com
 
 
 --
--- TOC entry 3725 (class 2604 OID 16445)
 -- Name: labels id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -282,126 +254,126 @@ ALTER TABLE ONLY public.labels ALTER COLUMN id SET DEFAULT nextval('public.label
 
 
 --
--- TOC entry 3906 (class 0 OID 16389)
--- Dependencies: 220
 -- Data for Name: boards; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.boards (id, name, created_at) FROM stdin;
-1	board1	2026-04-27 20:34:15.144106
-2	board2	2026-04-27 20:34:59.076642
+9	Perfectly Normal List Of Things To Do	2026-06-05 14:34:29.047849
+10	poor board	2026-06-05 14:50:30.387403
 \.
 
 
 --
--- TOC entry 3913 (class 0 OID 16453)
--- Dependencies: 227
 -- Data for Name: card_labels; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.card_labels (card_id, label_id) FROM stdin;
+18	5
+20	6
+17	6
+17	8
+16	5
+16	9
+19	5
+16	6
+20	9
+20	10
 \.
 
 
 --
--- TOC entry 3910 (class 0 OID 16420)
--- Dependencies: 224
 -- Data for Name: cards; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.cards (id, column_id, title, description, due_date, "position", created_at) FROM stdin;
-13	13	card1	\N	\N	100	2026-06-01 09:36:17.739143
-14	13	card2	\N	\N	200	2026-06-01 09:36:17.753867
-1	1	Test card	\N	\N	1	2026-04-27 21:09:49.577976
-11	1	test-card	\N	\N	2	2026-06-01 09:33:56.932913
-12	1	test-card-2	\N	\N	3	2026-06-01 09:34:11.843515
-15	1	new-card	\N	\N	4	2026-06-01 10:13:33.034623
+25	21	or comments		\N	4	2026-06-05 14:53:06.638726
+26	21	all budget went into the first one		\N	5	2026-06-05 14:53:34.779135
+19	19	Check for XSS		\N	2	2026-06-05 14:43:57.87814
+16	19	Do the Docker thingy	I guess it's better than Postgresing with 5 different versions on raw metal, but we gotta download more RAAAAM	2026-06-06	1	2026-06-05 14:39:08.899537
+17	18	Go to sleep	Feels good this whole sleeping thing. Watching Spider-Noir til 2 AM feels better tho-	2026-06-04	1	2026-06-05 14:41:06.06595
+18	15	Go look for more bugs	Naaaaaahhh, it's fiiiiiineeee, nothing will explooooode	2026-06-09	1	2026-06-05 14:43:03.687727
+20	15	Study for INF04	I swear to HolyC, if there's PySide I'mma look for an open window	2026-06-15	2	2026-06-05 14:46:16.562457
+24	20	hello there		\N	1	2026-06-05 14:52:39.980643
+21	21	we don't have money for capital letters too		\N	1	2026-06-05 14:51:35.536968
+22	21	and for labels either		\N	2	2026-06-05 14:52:25.655483
+23	21	or descriptions		\N	3	2026-06-05 14:52:34.843268
 \.
 
 
 --
--- TOC entry 3908 (class 0 OID 16400)
--- Dependencies: 222
 -- Data for Name: columns; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.columns (id, board_id, name, "position", created_at) FROM stdin;
-2	1	column2	100	2026-04-27 20:38:48.563709
-1	1	column1	200	2026-04-27 20:37:06.302596
-12	1	test-col	300	2026-06-01 09:33:56.893092
-13	1	new-col	400	2026-06-01 09:36:17.69616
-14	1	another-col	500	2026-06-01 09:36:17.718399
+15	9	Will anyone even bother?	100	2026-06-05 14:35:34.554543
+16	9	Wth, something's happening?	1100	2026-06-05 14:35:47.576636
+18	9	In progress, how exciting!	2100	2026-06-05 14:36:32.262938
+19	9	You thought we test things? Done!	3100	2026-06-05 14:36:43.258209
+21	10	second poor column (we don't have money for a third one)	1100	2026-06-05 14:51:09.613324
+20	10	first poor column	100	2026-06-05 14:50:49.453315
 \.
 
 
 --
--- TOC entry 3915 (class 0 OID 16485)
--- Dependencies: 229
 -- Data for Name: comments; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.comments (id, card_id, content, created_at) FROM stdin;
+4	19	<b>Hello there</b>	2026-06-05 14:44:52.533192
+5	19	<script>alert("I'm better than that, yk")</script>	2026-06-05 14:45:18.375736
+6	17	My educated research provided me with a conclusion that sleep is solely voluntary and can be substituted with a high enough amount of cola	2026-06-05 14:56:34.87608
 \.
 
 
 --
--- TOC entry 3912 (class 0 OID 16442)
--- Dependencies: 226
 -- Data for Name: labels; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.labels (id, name, color, board_id, created_at) FROM stdin;
-4	urgent	#ff0000	1	2026-05-28 13:21:10.988391
+5	skul	#0061ff	9	2026-06-05 14:54:04.624866
+6	actually importante	#ff4013	9	2026-06-05 14:54:18.841168
+8	not skul	#232323	9	2026-06-05 14:54:58.900027
+9	programien	#00f900	9	2026-06-05 14:57:06.956937
+10	kinda skul	#ff40ff	9	2026-06-05 14:57:54.082308
 \.
 
 
 --
--- TOC entry 3926 (class 0 OID 0)
--- Dependencies: 219
 -- Name: boards_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.boards_id_seq', 8, true);
+SELECT pg_catalog.setval('public.boards_id_seq', 10, true);
 
 
 --
--- TOC entry 3927 (class 0 OID 0)
--- Dependencies: 223
 -- Name: cards_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.cards_id_seq', 15, true);
+SELECT pg_catalog.setval('public.cards_id_seq', 26, true);
 
 
 --
--- TOC entry 3928 (class 0 OID 0)
--- Dependencies: 221
 -- Name: columns_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.columns_id_seq', 14, true);
+SELECT pg_catalog.setval('public.columns_id_seq', 21, true);
 
 
 --
--- TOC entry 3929 (class 0 OID 0)
--- Dependencies: 228
 -- Name: comments_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.comments_id_seq', 2, true);
+SELECT pg_catalog.setval('public.comments_id_seq', 6, true);
 
 
 --
--- TOC entry 3930 (class 0 OID 0)
--- Dependencies: 225
 -- Name: labels_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.labels_id_seq', 4, true);
+SELECT pg_catalog.setval('public.labels_id_seq', 10, true);
 
 
 --
--- TOC entry 3730 (class 2606 OID 16398)
 -- Name: boards boards_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -410,7 +382,6 @@ ALTER TABLE ONLY public.boards
 
 
 --
--- TOC entry 3747 (class 2606 OID 16459)
 -- Name: card_labels card_labels_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -419,7 +390,6 @@ ALTER TABLE ONLY public.card_labels
 
 
 --
--- TOC entry 3737 (class 2606 OID 16435)
 -- Name: cards cards_column_id_position_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -428,7 +398,6 @@ ALTER TABLE ONLY public.cards
 
 
 --
--- TOC entry 3739 (class 2606 OID 16433)
 -- Name: cards cards_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -437,7 +406,6 @@ ALTER TABLE ONLY public.cards
 
 
 --
--- TOC entry 3732 (class 2606 OID 16413)
 -- Name: columns columns_board_id_position_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -446,7 +414,6 @@ ALTER TABLE ONLY public.columns
 
 
 --
--- TOC entry 3734 (class 2606 OID 16411)
 -- Name: columns columns_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -455,7 +422,6 @@ ALTER TABLE ONLY public.columns
 
 
 --
--- TOC entry 3750 (class 2606 OID 16497)
 -- Name: comments comments_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -464,7 +430,6 @@ ALTER TABLE ONLY public.comments
 
 
 --
--- TOC entry 3743 (class 2606 OID 16481)
 -- Name: labels labels_board_id_name_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -473,7 +438,6 @@ ALTER TABLE ONLY public.labels
 
 
 --
--- TOC entry 3745 (class 2606 OID 16450)
 -- Name: labels labels_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -482,7 +446,6 @@ ALTER TABLE ONLY public.labels
 
 
 --
--- TOC entry 3748 (class 1259 OID 16472)
 -- Name: idx_card_labels_label_id; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -490,7 +453,6 @@ CREATE INDEX idx_card_labels_label_id ON public.card_labels USING btree (label_i
 
 
 --
--- TOC entry 3740 (class 1259 OID 16471)
 -- Name: idx_cards_column_id; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -498,7 +460,6 @@ CREATE INDEX idx_cards_column_id ON public.cards USING btree (column_id);
 
 
 --
--- TOC entry 3735 (class 1259 OID 16470)
 -- Name: idx_columns_board_id; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -506,7 +467,6 @@ CREATE INDEX idx_columns_board_id ON public.columns USING btree (board_id);
 
 
 --
--- TOC entry 3751 (class 1259 OID 16503)
 -- Name: idx_comments_card_id; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -514,7 +474,6 @@ CREATE INDEX idx_comments_card_id ON public.comments USING btree (card_id);
 
 
 --
--- TOC entry 3741 (class 1259 OID 16504)
 -- Name: idx_labels_board_id; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -522,7 +481,6 @@ CREATE INDEX idx_labels_board_id ON public.labels USING btree (board_id);
 
 
 --
--- TOC entry 3755 (class 2606 OID 16460)
 -- Name: card_labels card_labels_card_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -531,7 +489,6 @@ ALTER TABLE ONLY public.card_labels
 
 
 --
--- TOC entry 3756 (class 2606 OID 16465)
 -- Name: card_labels card_labels_label_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -540,7 +497,6 @@ ALTER TABLE ONLY public.card_labels
 
 
 --
--- TOC entry 3753 (class 2606 OID 16436)
 -- Name: cards cards_column_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -549,7 +505,6 @@ ALTER TABLE ONLY public.cards
 
 
 --
--- TOC entry 3752 (class 2606 OID 16414)
 -- Name: columns columns_board_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -558,7 +513,6 @@ ALTER TABLE ONLY public.columns
 
 
 --
--- TOC entry 3757 (class 2606 OID 16498)
 -- Name: comments comments_card_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -567,7 +521,6 @@ ALTER TABLE ONLY public.comments
 
 
 --
--- TOC entry 3754 (class 2606 OID 16475)
 -- Name: labels labels_board_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -575,11 +528,9 @@ ALTER TABLE ONLY public.labels
     ADD CONSTRAINT labels_board_id_fkey FOREIGN KEY (board_id) REFERENCES public.boards(id) ON DELETE CASCADE;
 
 
--- Completed on 2026-06-02 08:44:26 CEST
-
 --
 -- PostgreSQL database dump complete
 --
 
-\unrestrict NamcZzHDp6IQ2aYfEvL4P4P5JJBpzDuRSiueDgdCX9LNIUCns4YhtZhcUSeMRqV
+\unrestrict 8pYyrCiRPHcwhRS1JkDfQtzaqicIec3sYuznhSSuzK7IRefNr6U3UJU5hza5O8N
 
